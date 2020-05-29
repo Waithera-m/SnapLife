9df2abaa0ae1e9810e847dff271ad66381624874
Mary Waithera <njihiamary11@gmail.com>
2020-05-29 15:53:17 +0300

    Create base template from which all templates will inherit their html structure and create a template to handle the landing page; Create a view function to render the landing page, create urls file in which all url patterns to different views in the app will be defined and include this file in the project

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    """
+    view function renders the landing page
+    """
+    return render(request, 'profile_templates/index.html')
