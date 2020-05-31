a9e652a150206dccd685db537d44a90a9d469848
Mary Waithera <njihiamary11@gmail.com>
2020-05-31 17:02:27 +0300

    Add configurations to facilitate image uploads

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'profiles'
 
@@ -7,3 +9,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('', views.index, name='index'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
