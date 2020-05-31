c0861535540e4e16251039138cfeb1d6da355e9f
Mary Waithera <njihiamary11@gmail.com>
2020-05-31 16:23:09 +0300

    Create profile model to facilitate the creation of profile objects: create the model's save, update, and delete methods and their corresponding test functions

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from  django.contrib.auth.models import User
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+    """
+    class facilitates the creation of profile objects
+    """
+    bio = models.CharField(max_length=70)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='profiles/')
+    def __str__(self):
+        """
+        function returns informal representations of the models' objects
+        """
+        return self.user
+
+    def save_profile(self):
+        """
+        method saves entered profiles to the database
+        """
+        self.save()
+
+    def update_profile(self, using=None, fields=None, **kwargs):
+        """
+        method updates saved profile
+        """
+        if fields is not None:
+            fields = set(fields)
+            deferred_fields = self.get_deferred_fields()
+            if fields.intersection(deferred_fields):
+                fields = fields.union(deferred_fields)
+        super().refresh_from_db(using, fields, **kwargs)
+
+    def delete_profile(self):
+        """
+        method deletes saved profile
+        """
+        self.delete()
+        
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,49 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class  ProfileModelTests(TestCase):
+    """
+    class facilitates the creation of test units to test profile model's behavior
+    """
+    def setUp(self):
+        """
+        method defines the instructions to be executed before each test
+        """
+        self.new_user = User(username="peaches", email="njihiamary11@gmail.com", password="somepassword")
+        self.new_user.save()
+        self.new_profile = Profile(bio="something something scifi", user=self.new_user, profile_pic="tablet.jpg")
+    
+    def test_instance(self):
+        """
+        method checks if model's objesct are initialized properly
+        """
+        self.assertTrue(isinstance(self.new_profile, Profile))
+
+    def test_save_profile(self):
+        """
+        test unit tests if the model's object are saved to the database
+        """
+        self.new_profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+    
+    def test_update_profile(self):
+        """
+        method tests model's update profile functionality
+        """
+        self.new_profile.save_profile()
+        Profile.objects.filter(pk=self.new_profile.pk).update(bio="randomness")
+        self.new_profile.update_profile()
+        self.assertEqual(self.new_profile.bio, 'randomness')
+    
+    def test_delete_profile(self):
+        """
+        method tests model's delete functionality
+        """
+        self.new_profile.save_profile()
+        Profile.objects.filter(pk=self.new_profile.pk).delete()
+        self.new_profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
